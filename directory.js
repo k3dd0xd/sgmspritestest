5535bc259a78649585cb69f7aa16654c97e182b8
k3dd0xd <kudd.egrrr@gmail.com>
2024-11-15 23:29:01 +0700

    Update directory.js

--- directory.js
+++ directory.js
@@ -581,6 +581,7 @@ var ids = {
         "custom/k3dd0/filia",
         "custom/k3dd0/filia2",
         "custom/k3dd0/filia3",
-        "custom/k3dd0/marie2"
+        "custom/k3dd0/marie2",
+        "custom/ogo80/filia"
     ]
 };
