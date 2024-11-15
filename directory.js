1450468f7476328f95aa7010efaef01644a12583
k3dd0xd <kudd.egrrr@gmail.com>
2024-11-15 18:45:43 +0700

    Update directory.js

--- directory.js
+++ directory.js
@@ -575,6 +575,7 @@ var ids = {
         "Valentine_Wetwork"
     ],
     "custom": [
+        "custom/k3dd0/annie",
         "custom/k3dd0/filia",
         "custom/k3dd0/filia2",
         "custom/k3dd0/filia3"
