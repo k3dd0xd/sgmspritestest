985d34d65a9c51215e1f9271781d487cee7b0444
k3dd0xd <kudd.egrrr@gmail.com>
2024-11-28 02:17:38 +0700

    Update directory.js

--- directory.js
+++ directory.js
@@ -576,6 +576,7 @@ var ids = {
     ],
     "custom": [
         "custom/badsprite/eliza",
+        "custom/badsprite/filia",
         "custom/badsprite/marie",
         "custom/k3dd0/annie",
         "custom/k3dd0/annie2",
