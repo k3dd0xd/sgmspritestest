3a505925df3c6d232aa40a62668514bacddb26c1
k3dd0xd <kudd.egrrr@gmail.com>
2024-11-28 23:36:12 +0700

    revision

--- index.js
+++ index.js
@@ -149,17 +149,12 @@ function updateFlags() {
                     var y = Math.ceil(i / 4 / canvas.width);
                     var texturemap = texture[chowderlog[activechar][cid]];
                     var k = 4 * ((x % texturemap.width) + (y % texturemap.height) * texturemap.width);
-                    if (chowderlog[activechar][cid] == 8) { /* umbrella_veins */
+                    if (chowderlog[activechar][cid] == 5) { /* umbrella_veins */
                         newdata.data[i] = blend[moed](colormap[j] + texturemap.data[k], detail, line);
                         newdata.data[i + 1] = blend[moed](colormap[j + 1] + texturemap.data[k], detail, line);
                         newdata.data[i + 2] = blend[moed](colormap[j + 2] + texturemap.data[k], detail, line);
                     }
-                    else if (chowderlog[activechar][cid] == 9) { /* bunny_fishnet */
-                        newdata.data[i] = blend[moed](colormap[j] * texturemap.data[k] / 0xff, detail, line);
-                        newdata.data[i + 1] = blend[moed](colormap[j + 1] * texturemap.data[k + 1] / 0xff, detail, line);
-                        newdata.data[i + 2] = blend[moed](colormap[j + 2] * texturemap.data[k + 2] / 0xff, detail, line);
-                                            }
-                    else if (chowderlog[activechar][cid] == 10) { /* dot */
+                    else if (chowderlog[activechar][cid] == 6) { /* bunny_fishnet */
                         newdata.data[i] = blend[moed](colormap[j] * texturemap.data[k] / 0xff, detail, line);
                         newdata.data[i + 1] = blend[moed](colormap[j + 1] * texturemap.data[k + 1] / 0xff, detail, line);
                         newdata.data[i + 2] = blend[moed](colormap[j + 2] * texturemap.data[k + 2] / 0xff, detail, line);
@@ -459,12 +454,8 @@ function initTextures() {
     loadTexture(2, "texture/fire.png");
     loadTexture(3, "texture/water.png");
     loadTexture(4, "texture/killarainbow.png");
-    loadTexture(5, "texture/Rainbow.png");
-    loadTexture(6, "texture/Rainbow2.png");
-    loadTexture(7, "texture/blackspace.png");
-    loadTexture(8, "texture/umbrella_veins.png");
-    loadTexture(9, "texture/bunny_fishnet.png");
-    loadTexture(9, "texture/dot.png");
+    loadTexture(5, "texture/umbrella_veins.png");
+    loadTexture(6, "texture/bunny_fishnet.png");
 }
 
 /* Right Section */
@@ -613,7 +604,7 @@ function initSwatch(n, r, g, b, a) {
     }
 
     function updateChowder() {
-        if (chowderlog[activechar] && chowderlog[activechar][n] && [1, 2, 3, 4, 5, 6, 7].includes(chowderlog[activechar][n])) {
+        if (chowderlog[activechar] && chowderlog[activechar][n] && [1, 2, 3, 4].includes(chowderlog[activechar][n])) {
             color.disabled = true;
             text.disabled = true;
         }
