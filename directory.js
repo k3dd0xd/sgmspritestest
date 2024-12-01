9e8c28caaae4c08efb301c09e8f3d937d85d9417
k3dd0xd <kudd.egrrr@gmail.com>
2024-12-02 03:03:00 +0700

    Update directory.js

--- directory.js
+++ directory.js
@@ -623,6 +623,7 @@ var ids = {
         "custom/k3dd0/squigly5",
         "custom/k3dd0/umbrella7",
         "custom/ogo80/filia",
+        "custom/zxc/parasoul2",
         "custom/zxc/cat"
     ]
 };
