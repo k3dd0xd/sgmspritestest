8b495e084c13ebc587fa7f70430bd0390f90ff71
k3dd0xd <kudd.egrrr@gmail.com>
2024-12-01 23:41:13 +0700

    +

--- directory.js
+++ directory.js
@@ -613,6 +613,7 @@ var ids = {
         "custom/k3dd0/minette2",
         "custom/k3dd0/minette3",
         "custom/k3dd0/msfortune",
+        "custom/k3dd0/mrsvictoria",
         "custom/k3dd0/parasoul",
         "custom/k3dd0/parasoul2",
         "custom/k3dd0/parasoul3",
